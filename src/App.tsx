4ce0a938fa230863f70d0c20a0b899a3554a2184
Sergio k <valodya.kirik@gmail.com>
2022-11-01 13:34:01 +0200

    added my project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,33 +8,59 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+interface State {
+  hasClock: boolean;
+  clockName: string;
+}
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+export class App extends Component<{}, State> {
+  state: State = {
+    hasClock: true,
+    clockName: 'Clock-0',
+  };
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+  timerId = 0;
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+  }
 
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
+  componentDidUpdate(_: {}, prevState: State) {
+    const { clockName, hasClock } = this.state;
 
-        {' time is '}
+    if (clockName !== prevState.clockName && hasClock) {
+      window.console.debug(`Renamed from ${prevState.clockName} to ${clockName}`);
+    }
+  }
 
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { clockName, hasClock } = this.state;
+
+    return (
+      <div className="app">
+        <div className="app__content">
+          <h1>React clock</h1>
+
+          {hasClock && <Clock name={clockName} />}
+          <button
+            className="button"
+            type="button"
+            onClick={() => {
+              this.setState((currentState) => ({
+                hasClock: !currentState.hasClock,
+              }));
+            }}
+          >
+            Touch me
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
